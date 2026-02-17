ae7d7c1b9520b4bbacffdf7280a2097ff4435779
MasonJSwan <124463473+MasonJSwan@users.noreply.github.com>
2026-02-16 20:52:59 -0700

    [#34] Implement test.sh (#42)

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{mjs,cjs,jsx}"], 
+    plugins: { 
+      react,
+      js,
+      "@stylistic": stylistic
+    }, 
+    extends: ["js/recommended"], 
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser
+      },    
+    },
+    rules: {
+      // Force 2 spaces for indents
+      "@stylistic/indent": ["error", 2],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    }
+  },
+  { 
+    files: ["**/*.js"], 
+    plugins: {
+      js,
+      "@stylistic": stylistic
+    }, 
+    languageOptions: { 
+      sourceType: "commonjs" 
+    },
+    rules: {
+      // Force 2 spaces for indents
+      "@stylistic/indent": ['error', 2]
+    }
+  },
+  { 
+    files: ["**/*.css"], 
+    plugins: { css }, 
+    language: "css/css", 
+    extends: ["css/recommended"] 
+  },
+]);
