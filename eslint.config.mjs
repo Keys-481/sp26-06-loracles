db368bd57f0df3d4d6fdd79d6b9378203ad180f3
MasonJSwan <masonjswan@gmail.com>
2026-02-17 14:48:53 -0700

    one last try to fix css styling

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
+import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 import stylistic from "@stylistic/eslint-plugin";
 
@@ -54,5 +55,11 @@ export default defineConfig([
       // No unnecessary semicolons
       "@stylistic/no-extra-semi": "error"
     }
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"]
   }
 ]);
