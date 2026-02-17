564f445eed9169dc06c5074373a48110ab00b676
MasonJSwan <masonjswan@gmail.com>
2026-02-16 20:10:55 -0700

    Testing added for linting and a test suite, with example

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,54 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
+import react from "eslint-plugin-react";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
   { 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
+    files: ["**/*.{mjs,cjs,jsx}"], 
     plugins: { 
+      react,
       js,
       "@stylistic": stylistic
     }, 
     extends: ["js/recommended"], 
-    languageOptions: { 
-      globals: globals.browser    
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser
+      },    
     },
     rules: {
+      // Force 2 spaces for indents
+      "@stylistic/indent": ["error", 2],
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    }
+  },
+  { 
+    files: ["**/*.js"], 
+    plugins: {
+      js,
+      "@stylistic": stylistic
+    }, 
+    languageOptions: { 
+      sourceType: "commonjs" 
+    },
+    rules: {
+      // Force 2 spaces for indents
       "@stylistic/indent": ['error', 2]
     }
   },
-  pluginReact.configs.flat.recommended,
   { 
     files: ["**/*.css"], 
-    plugins: { 
-      css,
-      "@stylistic": stylistic
-    }, 
+    plugins: { css }, 
     language: "css/css", 
-    extends: ["css/recommended"],
-    rules: {
-      "@stylistic/indent": ['error', 2]
-    }
+    extends: ["css/recommended"] 
   },
 ]);
--- test.sh
+++ test.sh
@@ -1,2 +1,59 @@
 #!/bin/bash
-exit 127
+set -euo pipefail
+
+echo "Loracles: Test"
+
+# Check presence package.json
+if [ ! -f "package.json" ]; then
+  echo "Error: package.json not found in current directory"
+  exit 1
+fi
+
+echo "==Installing node=="
+echo "  npm ci"
+
+# Run npm ci, and save the status to a variable
+out_npm_install=$(npm ci) 
+status=$?
+# If failed, error out
+if [[ "$status" != 0 ]]; then
+  echo "  :( Error: Failed to install npm. Returned $status"
+  echo "==Output of npm ci=="
+  echo "$out_npm_install"
+  exit 2
+fi
+
+echo "  :) Success!"
+
+echo "==Testing=="
+echo "  npm run test"
+
+# Run npm run test, and save the status to a variable
+out_npm_run_test=$(npm run test) 
+status=$?
+# If failed, error out
+if [[ "$status" != 0 ]]; then
+  echo "  :( Testing issues"
+  echo "==Output of npm run test=="
+  echo "$out_npm_run_test"
+  exit 3
+fi
+
+echo "  :) Success!"
+
+echo "==Linting=="
+echo "  npm run lint"
+
+# Run npm run lint, and save the status to a variable
+out_npm_run_lint=$(npm run lint) 
+status=$?
+# If failed, error out
+if [[ "$status" != 0 ]]; then
+  echo "  :( Linting issues"
+  echo "==Output of npm run lint=="
+  echo "$out_npm_run_lint"
+  exit 4
+fi
+
+echo "  :) Success!"
+exit 0
