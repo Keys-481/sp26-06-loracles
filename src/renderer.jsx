26f81f1b15b57f3e4897d505d13ec638b70ac26c
MasonJSwan <masonjswan@gmail.com>
2026-02-17 14:39:58 -0700

    Restore autofix, remove css linting, and add a bug for the linter to catch

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -28,7 +28,7 @@
 
 import './index.css';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App'; 
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
