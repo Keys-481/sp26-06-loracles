ff95759adc8368f8916ce52a21a8d5f01933b054
MasonJSwan <124463473+MasonJSwan@users.noreply.github.com>
2026-02-28 16:00:47 -0700

    [#62] Make button images display in app (#65)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import img_file from "./assets/file.png";
+import img_folder from "./assets/folder.png";
+import img_settings from "./assets/settings.png";
+import img_world from "./assets/world.png";
 
 function App() {
 
@@ -8,19 +12,19 @@ function App() {
         <div class="div1">
           <div class="buttonRow">
             <button type="button" class="button">
-              <img src="/src/assets/file.png" alt="Icon" class="icon"></img>
+              <img src={img_file} alt="Icon" class="icon"></img>
               <span>File Select</span>
             </button>
             <button type="button" class="button">
-              <img src="/src/assets/folder.png" alt="Icon" class="icon"></img>
+              <img src={img_folder} alt="Icon" class="icon"></img>
               <span>Folder Select</span>
             </button>
             <button type="button" class="button">
-              <img src="/src/assets/settings.png" alt="Icon" class="icon"></img>
+              <img src={img_settings} alt="Icon" class="icon"></img>
               <span>Settings</span>
             </button>
             <button type="button" class="button">
-              <img src="/src/assets/world.png" alt="Icon" class="icon"></img>
+              <img src={img_world} alt="Icon" class="icon"></img>
               <span>Language</span>
             </button>
           </div>
