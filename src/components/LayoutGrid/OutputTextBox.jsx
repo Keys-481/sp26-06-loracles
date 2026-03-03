5fd3b91c56d07b8b8a8c83a81dbbb6fccaf28137
MasonJSwan <masonjswan@gmail.com>
2026-03-03 16:40:47 -0700

    Added nice documentation

--- src/components/LayoutGrid/OutputTextBox.jsx
+++ src/components/LayoutGrid/OutputTextBox.jsx
@@ -3,7 +3,9 @@
  *
  * This will be where the text scanned from a document will be placed.
  *
- * @returns
+ * @param {Object} param0 Container for component props
+ * @param {*} param0.labelText Fills in the text of the label for the textarea
+ * @param {string} [param0.placeholder=""] Fills in the placeholder attribute of the textarea
  */
 function OutputTextBox({ labelText, placeholder="" }) {
   // Ensure function props are of the right type
@@ -11,8 +13,6 @@ function OutputTextBox({ labelText, placeholder="" }) {
     throw new TypeError(`Expected a string, got ${typeof labelText}`);
   if (typeof placeholder !== "string")
     throw new TypeError(`Expected a string, got ${typeof placeholder}`);
-
-  // Ensure function props don't have invalid values
   if (labelText.trim() === "")
     throw new Error(`Expected non-empty string`);
 
