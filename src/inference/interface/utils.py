0d7c74bec0a301e85d8e902db1ce7cddf2f05897
porterrigby <prigby.a@gmail.com>
2026-03-07 16:37:01 -0700

    Implements HTRModel interface and utils.

--- src/inference/interface/utils.py
+++ src/inference/interface/utils.py
@@ -4,6 +4,8 @@ from typing import NamedTuple, List
 PolygonPoint = NamedTuple('Point', [('x', int), ('y', int)])
 Polygon = NamedTuple('Polygon', [('points', List[PolygonPoint])])
 
+TextAnnotation = NamedTuple('TextAnnotation', [('text', str), ('polygon', Polygon)])
+
 
 @dataclass
 class LineSegmentationOutput:
@@ -13,3 +15,13 @@ class LineSegmentationOutput:
         for poly in self.polygons:
             if not isinstance(poly, Polygon):
                 raise TypeError(f"Expected Polygon, got {type(poly)}")
+
+
+@dataclass
+class TextRecognitionOutput:
+    annotations: List[TextAnnotation]
+
+    def __post_init__(self):
+        for annotation in self.annotations:
+            if not isinstance(annotation, TextAnnotation):
+                raise TypeError(f"Expected TextAnnotation, got {type(annotation)}")
