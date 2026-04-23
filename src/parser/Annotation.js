18f6c8bd511ad58b0ff0e08e36b82e599ecb043e
MasonJSwan <masonjswan@gmail.com>
2026-04-23 13:08:28 -0600

    Documented Annotation

--- src/parser/Annotation.js
+++ src/parser/Annotation.js
@@ -1,29 +1,58 @@
 import ImageBounds from './ImageBounds';
 
+/**
+ * Class representing a line of text bounded by a rectangle
+ */
 class Annotation {
   #line;
   #bounds;
 
+  /**
+   * Create an annotation
+   * @param {String} line - Line of text 
+   * @param {Number[]} nw - x and y of top left corner of bounding rectangle
+   * @param {Number[]} se - x and y of bottom right corner of bounding rectangle
+   */
   constructor(line, nw, se) {
     [this.#line, this.#bounds] = [line, new ImageBounds(nw, se)];
   }
 
+  /**
+   * Get the line
+   * @return {String} the line
+   */
   get line() {
     return this.#line;
   }
 
+  /**
+   * Get the bounding box
+   * @return {ImageBounds} the bounding box
+   */
   get bounds() {
     return this.#bounds;
   }
 
+  /**
+   * Set the line
+   * @param {String} value - new value to set the line to
+   */
   set line(value) {
     this.#line = value;
   }
 
+  /**
+   * Set the bounds
+   * @param {ImageBounds} value - new value to set the bounds to
+   */
   set bounds(value) {
     this.#bounds = value;
   }
 
+  /**
+   * Print the bounds and line of this annotation
+   * @returns {String}
+   */
   toString() {
     return `${this.#bounds}: ${this.#line}`;
   }
