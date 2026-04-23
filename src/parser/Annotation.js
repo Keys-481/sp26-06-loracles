098ed0abe2d8b4d4086554b1d231031c97d3e0a4
MasonJSwan <masonjswan@gmail.com>
2026-04-23 13:19:09 -0600

    Fixed wrong type and added a little more

--- src/parser/Annotation.js
+++ src/parser/Annotation.js
@@ -5,16 +5,19 @@ import ImageBounds from './ImageBounds';
  */
 class Annotation {
   /**
-   * @type {String}
+   * @type String
    */
   #line;
   /**
-   * @type {ImageBounds}
+   * @type ImageBounds
    */
   #bounds;
 
   /**
    * Create an annotation
+   *
+   * nw and se can be TypedArrays or Arrays of numbers
+   *
    * @param {String} line - Line of text
    * @param {Number[]} nw - x and y of top left corner of bounding rectangle
    * @param {Number[]} se - x and y of bottom right corner of bounding rectangle
